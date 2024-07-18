f8526c5e61db2c35cb202f97a59758daea301482
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-18 11:30:35 -0300

    chore: add src folder and update paths

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,16 +2,14 @@ import {
   LucideProps,
   Moon,
   SunMedium,
-  Twitter,
   type Icon as LucideIcon,
 } from "lucide-react"
 
-export type Icon = LucideIcon
+export type Icon = typeof LucideIcon
 
 export const Icons = {
   sun: SunMedium,
   moon: Moon,
-  twitter: Twitter,
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
