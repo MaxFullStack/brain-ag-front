6b0dfc58ecf34eed4ac169bf0ccc5b93f3c21453
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-18 12:26:16 -0300

    chore: update site info

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,18 +1,19 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "Brain Ag",
+  description: "Brain Ag - Front-end technical test",
   mainNav: [
     {
       title: "Home",
       href: "/",
     },
+    {
+      title: "About",
+      href: "/about",
+    },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    github: "https://github.com/MaxFullStack",
   },
 }
