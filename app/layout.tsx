e5459f34d32d1efa8816a52616786c8367aa84cf
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-18 12:28:58 -0300

    chore: add layout folder and update paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import "@/styles/globals.css"
+
 import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeProvider } from "@/components/theme-provider"
+import { SiteHeader } from "@/components/layout/site-header"
+import { TailwindIndicator } from "@/components/layout/tailwind-indicator"
+import { ThemeProvider } from "@/components/layout/theme-provider"
 
 export const metadata: Metadata = {
   title: {
-    default: siteConfig.name,
+    default: `${siteConfig.name} | Front-end`,
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
---
+++ src/components/layout/site-header.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { useState } from "react"
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+
+import MainNav from "./main-nav"
+import MobileNav from "./mobile-nav"
+import { ThemeToggle } from "./theme-toggle"
+
+export function SiteHeader() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b">
+      <div className="flex h-16 items-center space-x-16 px-4 sm:justify-between sm:space-x-0 md:px-16">
+        <Link href="/" className="flex items-center space-x-2 md:mr-16">
+          <Icons.logo className="size-6 pb-0.5" />
+          <div className="inline-block text-xl font-bold">
+            {siteConfig.name}
+          </div>
+        </Link>
+        <div className="hidden md:flex">
+          <MainNav items={siteConfig.mainNav} />
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-4">
+            <ThemeToggle />
+            <Button
+              className="flex md:hidden"
+              variant="outline"
+              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            >
+              <span className="sr-only">Open main menu</span>
+              {mobileMenuOpen ? (
+                <Icons.x className="size-6" />
+              ) : (
+                <Icons.menu className="size-6" />
+              )}
+            </Button>
+          </nav>
+        </div>
+      </div>
+      <MobileNav
+        open={mobileMenuOpen}
+        onClose={() => setMobileMenuOpen(false)}
+      />
+    </header>
+  )
+}
