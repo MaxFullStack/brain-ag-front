0ba481c8d6dad62481199095c3dd124aa4e8f746
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-24 20:29:49 -0300

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
