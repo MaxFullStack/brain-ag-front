444cf92612386df27d28aa16c4118a537f09bf0c
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-24 20:29:06 -0300

    feat: add project details in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,73 @@
-import Link from "next/link"
+import React from "react"
+import {
+  businessRequirements,
+  businessRequirementsTitle,
+  pageTitle,
+  producerFields,
+  technicalRequirements,
+  technicalRequirementsTitle,
+  testObjective,
+} from "@/constants/test-info"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import FooterSection from "@/components/layout/footer-section"
+import { SiteHeader } from "@/components/layout/site-header"
 
-export default function IndexPage() {
+const IndexPage = () => {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    <>
+      <SiteHeader />
+      <main id="solutions" className="px-8 py-8 md:px-12 md:py-10">
+        <h1 className="mb-8 text-center text-3xl font-bold">{pageTitle}</h1>
+        <p className="mb-2">{testObjective}</p>
+        <ul className="mb-8 list-inside list-disc">
+          {producerFields.map((field: string, index: number) => (
+            <li key={index}>{field}</li>
+          ))}
+        </ul>
+        <h2 className="mb-2 text-2xl font-bold">{businessRequirementsTitle}</h2>
+        <ul className="mb-8 list-inside list-disc">
+          {businessRequirements.map((requirement: string, index: number) => (
+            <li key={index}>{requirement}</li>
+          ))}
+        </ul>
+        <h2 className="mb-4 text-2xl font-bold">
+          {technicalRequirementsTitle}
+        </h2>
+        <div className="mb-4">
+          <h3 className="mb-2 text-xl font-bold">Front-end:</h3>
+          <ul className="mb-8 list-inside list-disc">
+            {technicalRequirements.frontEnd.map(
+              (req: string, index: number) => (
+                <li key={index}>{req}</li>
+              )
+            )}
+          </ul>
+          <h3 className="mb-2 text-xl font-bold">Back-end:</h3>
+          <ul className="mb-8 list-inside list-disc">
+            {technicalRequirements.backEnd.map((req: string, index: number) => (
+              <li key={index}>{req}</li>
+            ))}
+          </ul>
+          <h3 className="mb-2 text-xl font-bold">Desejável:</h3>
+          <ul className="mb-8 list-inside list-disc">
+            {technicalRequirements.desirable.map(
+              (req: string, index: number) => (
+                <li key={index}>{req}</li>
+              )
+            )}
+          </ul>
+          <h3 className="mb-2 text-xl font-bold">Bônus:</h3>
+          <ul className="mb-8 list-inside list-disc">
+            {technicalRequirements.bonus.map((req: string, index: number) => (
+              <li key={index}>{req}</li>
+            ))}
+          </ul>
+        </div>
+        <p className="mb-4">{technicalRequirements.fullStack}</p>
+      </main>
+      <FooterSection />
+    </>
   )
 }
+
+export default IndexPage
